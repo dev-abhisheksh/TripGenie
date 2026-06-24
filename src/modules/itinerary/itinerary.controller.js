fd25fe73736d280e83ca6047e7e2aece31156f00
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-06-24 21:52:39 +0530

    refactor uploadDocument to use extractText utility for improved PDF and image text extraction

--- src/modules/itinerary/itinerary.controller.js
+++ src/modules/itinerary/itinerary.controller.js
@@ -3,6 +3,8 @@ import ApiError from "../../utils/apiError.js";
 import uploadToCloudinary from "../../utils/uploadToCloudinary.js";
 import Tesseract from "tesseract.js";
 import { createRequire } from "module";
+import { extractTravelData } from "../../services/ai.service.js";
+import extractText from "../../utils/extractText.js";
 
 const require = createRequire(import.meta.url);
 const pdf = require("pdf-parse");
@@ -12,39 +14,23 @@ const uploadDocument = asyncHandler(async (req, res) => {
         throw new ApiError(400, "File is required");
     }
 
-    // Upload original file to Cloudinary
     const uploadResult = await uploadToCloudinary(
         req.file.buffer,
         "auto"
     );
 
-    let extractedText = "";
-
-    // PDF Extraction
-    if (req.file.mimetype === "application/pdf") {
-        const data = await pdf(req.file.buffer);
-        extractedText = data.text;
-    }
-
-    // Image OCR
-    else if (req.file.mimetype.startsWith("image/")) {
-        const result = await Tesseract.recognize(
-            req.file.buffer,
-            "eng"
-        );
-
-        extractedText = result.data.text;
-    }
-
-    else {
-        throw new ApiError(400, "Unsupported file type");
-    }
-
+    const extractedText = await extractText(req.file)
     console.log("Extracted Text:", extractedText);
+
+    // console.log("Extracted Text:", extractedText);
+
+    let aiResponse = await extractTravelData(extractedText)
+    aiResponse = JSON.parse(aiResponse)
+    console.log("Ai response:", aiResponse)
 
     res.status(200).json({
         success: true,
-        extractedText,
+        aiResponse,
         fileUrl: uploadResult.secure_url,
         publicId: uploadResult.public_id,
     });
