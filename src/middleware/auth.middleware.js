cc5f47ca799e28dea9c189ad660ace84150dd092
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-06-24 19:36:44 +0530

    add authentication middleware to verify access tokens

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,22 @@
+import ApiError from "../utils/apiError";
+import asyncHandler from "./asyncHandler.middleware";
+
+export const verifyToken = asyncHandler(async (req, res, next) => {
+    const token = req.cookies?.accessToken || req.headers.authorization?.replace("Bearer", "")
+    if (!token) throw new ApiError(401, "Unauthorized, No token provided")
+
+    try {
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+        const user = await Useer.findById(decoded._id).select("-password -refreshToken")
+        if (!user) throw new ApiError(401, "Unauthorized, Invalid token")
+
+        req.user = user
+        next();
+    } catch (error) {
+        if (error.name === "TokenExpiredError") {
+            throw new ApiError(401, "Unauthorized, Access token expired")
+        }
+        return next(new ApiError(401, "Unauthorized, Invalid token"))
+    }
+})
