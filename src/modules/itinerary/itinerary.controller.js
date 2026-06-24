633b6cc30744bf822dbc2b8fea13b28da900aef6
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-06-24 21:16:45 +0530

    add Tesseract.js integration for image OCR and enhance PDF extraction in uploadDocument

--- src/modules/itinerary/itinerary.controller.js
+++ src/modules/itinerary/itinerary.controller.js
@@ -1,6 +1,9 @@
 import asyncHandler from "../../middleware/asyncHandler.middleware.js";
+import ApiError from "../../utils/apiError.js";
 import uploadToCloudinary from "../../utils/uploadToCloudinary.js";
+import Tesseract from "tesseract.js";
 import { createRequire } from "module";
+
 const require = createRequire(import.meta.url);
 const pdf = require("pdf-parse");
 
@@ -9,21 +12,44 @@ const uploadDocument = asyncHandler(async (req, res) => {
         throw new ApiError(400, "File is required");
     }
 
-    const result = await uploadToCloudinary(
+    // Upload original file to Cloudinary
+    const uploadResult = await uploadToCloudinary(
         req.file.buffer,
         "auto"
     );
 
-    const data = await pdf(req.file.buffer)
-    console.log("Extracted DATA:", data.text)
+    let extractedText = "";
+
+    // PDF Extraction
+    if (req.file.mimetype === "application/pdf") {
+        const data = await pdf(req.file.buffer);
+        extractedText = data.text;
+    }
+
+    // Image OCR
+    else if (req.file.mimetype.startsWith("image/")) {
+        const result = await Tesseract.recognize(
+            req.file.buffer,
+            "eng"
+        );
+
+        extractedText = result.data.text;
+    }
+
+    else {
+        throw new ApiError(400, "Unsupported file type");
+    }
+
+    console.log("Extracted Text:", extractedText);
 
     res.status(200).json({
         success: true,
-        fileUrl: result.secure_url,
-        publicId: result.public_id,
+        extractedText,
+        fileUrl: uploadResult.secure_url,
+        publicId: uploadResult.public_id,
     });
 });
 
 export {
     uploadDocument
-}
+};
