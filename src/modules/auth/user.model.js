a8d49089ae285b195fa70e5d5762c388828536fa
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-06-24 19:36:00 +0530

    add user and itinerary models with schema definitions

---
+++ src/modules/auth/user.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+
+    fullName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        select: false
+    },
+
+    avatar: {
+        type: String,
+        default: ""
+    }
+
+}, { timestamps: true });
+
+export const User = mongoose.model("User", userSchema)
